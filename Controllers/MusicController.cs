a5866a7374bd5acd0c1371c817748d9647041283
Tannus Esquerdo <tandrad8@my.centennialcollege.ca>
2023-12-06 23:47:01 -0500

    fix: Add Controller, DTO and repository

---
+++ Controllers/MusicController.cs
@@ -0,0 +1,55 @@
+﻿// MusicController.cs
+using Microsoft.AspNetCore.Mvc;
+using MusicAPI.DTO;
+using MusicAPI.Repository;
+using MusicAPI;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class MusicController : ControllerBase
+{
+    private readonly MusicRepository _musicRepository;
+
+    public MusicController(MusicRepository musicRepository)
+    {
+        _musicRepository = musicRepository;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllMusic()
+    {
+        var allMusic = await _musicRepository.GetAllMusic();
+        var musicDtos = allMusic.Select(entity => new MusicDto
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            Artist = entity.Artist,
+            S3BucketKey = entity.S3BucketKey,
+            // Map other properties as needed
+        }).ToList();
+
+        return Ok(musicDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetMusicById(string id)
+    {
+        var music = await _musicRepository.GetMusicById(id);
+        if (music == null)
+        {
+            return NotFound();
+        }
+
+        var musicDto = new MusicDto
+        {
+            Id = music.Id,
+            Title = music.Title,
+            Artist = music.Artist,
+            S3BucketKey = music.S3BucketKey,
+            // Map other properties as needed
+        };
+
+        return Ok(musicDto);
+    }
+}
